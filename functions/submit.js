4a2f162d6cef17a775c6ba7c99924957be1674f1
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-12 13:04:25 -0400

    Test try

--- functions/submit.js
+++ functions/submit.js
@@ -2,25 +2,25 @@
  * POST /submit
  */
 export async function onRequestPost(request) {
-  // try {
+  try {
     // Get data from file upload.
     let data = await request.formData();
 
     // DEBUG
     console.log(data)
 
-    // Convert data to JSON string.
-    let json = JSON.stringify(data, null, 2);
-
-    // DEBUG
-    console.log(json)
+    // // Convert data to JSON string.
+    // let json = JSON.stringify(data, null, 2);
+    //
+    // // DEBUG
+    // console.log(json)
 
     return new Response(json, {
       headers: {
         'Content-Type': 'application/json;charset=utf-8'
       }
     });
-  // } catch (err) {
-  //   return new Response('Error parsing JSON content', { status: 400 });
-  // }
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
+  }
 }
