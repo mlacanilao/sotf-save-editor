fb1688d6c56936e9297c2be646e6384bce181a92
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-12 01:54:35 -0500

    Add engine

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express()
 const port = 443
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
   res.render("index"); // index refers to index.ejs
 })
