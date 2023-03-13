db26c9273b3cfb301eb7dfb8340e9517a72d4bdc
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-13 17:21:19 -0400

    Debug JSON

--- functions/submit.js
+++ functions/submit.js
@@ -8,8 +8,13 @@ export async function onRequestPost({request}) {
     return new Response('Only POST requests are allowed', {status: 405})
   }
 
-  // Get the FormData object from the request body
-  const formData = await request.formData()
+  // Get the boundary string from the content type header
+  const contentType = request.headers.get('Content-Type')
+  const boundary = contentType.match(/boundary=([^;]+)/)[1]
+  console.log('Boundary:', boundary)
+
+  // Parse the MIME multipart blob
+  const formData = await parseMultipart(request.body, boundary)
   console.log('FormData object:', formData)
 
   // Get the file from the FormData object using the name attribute of the file input field
@@ -42,4 +47,43 @@ export async function onRequestPost({request}) {
 
   // Return the JSON response
   return new Response(JSON.stringify(fileJson), {status: 200, headers: {'Content-Type': 'application/json'}})
+}
+
+async function parseMultipart(body, boundary) {
+  const reader = body.getReader()
+  const decoder = new TextDecoder()
+  let result = {}
+  let done = false
+  let buffer = ''
+
+  while (!done) {
+    const {value, done: readerDone} = await reader.read()
+    done = readerDone
+    buffer += decoder.decode(value, {stream: !done})
+
+    const parts = buffer.split(`--${boundary}`)
+    buffer = parts.pop()
+    for (const part of parts) {
+      if (!part.startsWith('\r\n')) {
+        continue
+      }
+
+      const contentStart = part.indexOf('\r\n\r\n') + 4
+      const headers = part.slice(2, contentStart - 4)
+      const content = part.slice(contentStart, -2)
+
+      const nameMatch = headers.match(/name="([^"]+)"/)
+      const filenameMatch = headers.match(/filename="([^"]+)"/)
+      if (filenameMatch) {
+        const filename = filenameMatch[1]
+        const typeMatch = headers.match(/Content-Type: (.+)/)
+        const type = typeMatch ? typeMatch[1] : 'application/octet-stream'
+        result[nameMatch[1]] = new File([content], filename, {type})
+      } else if (nameMatch) {
+        result[nameMatch[1]] = content
+      }
+    }
+  }
+
+  return new FormData(result)
 }
