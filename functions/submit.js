244ff025c9e287d029cdbceb0d314e54b4b041c2
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-12 23:43:17 -0400

    Debug JSON

--- functions/submit.js
+++ functions/submit.js
@@ -1,21 +1,35 @@
 /**
  * POST /submit
  */
-export async function onRequestPost(context) {
-  const { headers } = context.request;
+export async function onRequestPost(event) {
+  const { headers } = event.request;
   const contentType = headers.get('content-type') || '';
 
   // DEBUG
   console.log(`[DEBUG]: contentType = ${contentType}`)
 
   // DEBUG
-  console.log(`[DEBUG]: context = ${context}`)
+  console.log(`[DEBUG]: event = ${event}`)
 
   // Convert context to form data.
-  let data = await request.request.formData();
+  let data = await event.request.formData();
 
   // DEBUG
   console.log(`[DEBUG]: data = ${data}`)
+
+  // Get files?
+  let files = await event.request.files;
+
+  // Length
+  const numFiles = files.length;
+
+  // DEBUG
+  console.log(`[DEBUG]: numFiles = ${numFiles}`)
+
+  // Get first file?
+  let file = files[0]
+
+
 
   // Convert data string to JSON.
   let json = JSON.stringify([...data], null, 2);
