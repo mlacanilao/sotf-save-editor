e0b5f6da0e49fd557afdef7d94a2f027ab549ee3
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-13 00:08:40 -0400

    Debug JSON

--- functions/submit.js
+++ functions/submit.js
@@ -2,22 +2,17 @@
  * POST /submit
  */
 export async function onRequestPost(event) {
-  // const { headers } = event.request;
-  // const contentType = headers.get('content-type') || '';
-  // const request = event.request;
-  // const body = await request.clone().text();
-  //
-  // // DEBUG
-  // console.log(`[DEBUG]: event = ${event}`)
-  // console.log(`[DEBUG]: contentType = ${contentType}`)
-  // console.log(`[DEBUG]: request = ${request}`)
-  // console.log(`[DEBUG]: body = ${body}`)
-  //
+  // DEBUG
+  console.log(`[DEBUG]: context = ${event}`)
+
   // Convert context to form data.
   let data = await event.request.formData();
 
   // DEBUG
   console.log(`[DEBUG]: data = ${data}`)
+
+  // DEBUG
+  console.log(`[DEBUG]: data.values() = ${data.values()}`)
 
   // Convert data string to JSON.
   let json = JSON.stringify([...data], null, 2);
@@ -30,25 +25,4 @@ export async function onRequestPost(event) {
       'Content-Type': 'application/json;charset=utf-8',
     },
   });
-  // try {
-  //   // Get data from file upload.
-  //   let data = await request.formData();
-  //
-  //   // DEBUG
-  //   console.log(data)
-  //
-  //   // // Convert data to JSON string.
-  //   // let json = JSON.stringify(data, null, 2);
-  //   //
-  //   // // DEBUG
-  //   // console.log(json)
-  //
-  //   // return new Response(data, {
-  //   //   headers: {
-  //   //     'Content-Type': 'application/json;charset=utf-8'
-  //   //   }
-  //   // });
-  // } catch (err) {
-  //   return new Response('Error parsing JSON content', { status: 400 });
-  // }
 }
