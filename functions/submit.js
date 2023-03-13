b3efc6e90da1a57ca6752788ba15ae348d19d69a
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-13 17:43:44 -0400

    Debug JSON

--- functions/submit.js
+++ functions/submit.js
@@ -1,20 +1,22 @@
-async function handleRequest(request) {
-  if (request.method === 'POST') {
-    const formData = await request.formData()
-    const file = formData.get('file')
-    if (file) {
-      // do something with the file
-      const fileContents = await file.text()
-      console.log(fileContents)
-    } else {
-      // no file was uploaded
-    }
+export async function handleRequest({request}) {
+  try {
+    const formData = await request.formData();
+    const file = formData.get('file');
+
+    // Read the contents of the file
+    const fileContent = await file.text();
+
+    // Parse file content as JSON
+    const jsonData = JSON.parse(fileContent);
+
+    // Return JSON response
+    const jsonResponse = JSON.stringify(jsonData, null, 2);
+    return new Response(jsonResponse, {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    });
+  } catch (err) {
+    return new Response('Error parsing file content', { status: 400 });
   }
-
-  // handle other requests
-  return new Response('Hello, world!')
 }
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
