b41028f4321931e112ec1c12c3a108850c87ff02
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-13 17:37:14 -0400

    Debug JSON

--- functions/submit.js
+++ functions/submit.js
@@ -1,20 +1,20 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
 async function handleRequest(request) {
   if (request.method === 'POST') {
     const formData = await request.formData()
     const file = formData.get('file')
-
     if (file) {
+      // do something with the file
       const fileContents = await file.text()
-      // do something with the file contents
+      console.log(fileContents)
+    } else {
+      // no file was uploaded
     }
-
-    return new Response('File uploaded successfully')
   }
 
   // handle other requests
   return new Response('Hello, world!')
 }
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
