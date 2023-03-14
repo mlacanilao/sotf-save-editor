dc4627f7e1e80ee49cd0192aeb5ec40fc19b3229
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-13 23:12:04 -0400

    Debug JSON

--- public/main.js
+++ public/main.js
@@ -6,6 +6,7 @@ const selectedFileName = document.querySelector('#selectedFileName');
 fileInput.addEventListener('change', () => {
   // Get the name of the selected file
   const fileName = fileInput.files[0].name;
+  console.log(`Selected file: ${fileName}`);
 
   // Set the text content of the selected file name element to the selected file name
   selectedFileName.textContent = `Selected file: ${fileName}`;
