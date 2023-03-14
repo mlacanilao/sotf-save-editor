4587ac9175f5cce71307a6c7ffa06b39b379ba16
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-13 23:47:51 -0400

    Debug JSON

--- public/main.js
+++ public/main.js
@@ -21,7 +21,7 @@ fileInput.addEventListener('change', () => {
       console.log(`fileContent: ${fileContent}`);
 
       // Set the text content of the selected file name element to the selected file name
-      selectedFileName.textContent = `Selected file: ${fileContent}`;
+      // selectedFileName.textContent = `Selected file: ${fileContent}`;
 
       // Parse the contents as JSON
       const jsonData = JSON.parse(fileContent);
