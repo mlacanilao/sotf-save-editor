7394e5e716a1a1449147de82f42238109a715819
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-13 21:33:44 -0400

    Debug JSON

--- functions/submit.js
+++ functions/submit.js
@@ -1,17 +1,25 @@
 export async function onRequestPost({request}) {
   try {
-    // Read the contents of the file
-    console.log(`Reading file contents...`);
-    const fileContent = await request.arrayBuffer();
-    console.log(`fileContent: ${fileContent}`);
+    // Get the request body as an ArrayBuffer
+    const body = await request.arrayBuffer();
+    console.log(`body: ${body}`);
 
-    // Parse file content as JSON
-    console.log(`Parsing file contents as JSON...`);
-    const jsonData = JSON.parse(new TextDecoder().decode(fileContent));
+    // Convert the ArrayBuffer to a Uint8Array
+    const uint8Array = new Uint8Array(body);
+    console.log(`uint8Array: ${uint8Array}`);
+
+    // Convert the Uint8Array to a string using TextDecoder
+    const decoder = new TextDecoder();
+    const string = decoder.decode(uint8Array);
+    console.log(`string: ${string}`);
+
+    // Parse the string as JSON
+    const jsonData = JSON.parse(string);
 
     // Return JSON response
     const jsonResponse = JSON.stringify(jsonData, null, 2);
     console.log(`Returning JSON response: ${jsonResponse}`);
+
     return new Response(jsonResponse, {
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
