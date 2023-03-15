2d6c2349f305bde3cc56db9b6f4671893ba87fc7
mlacanilao <Michael.V.Lacanilao@gmail.com>
2023-03-14 23:32:03 -0400

    Debug JSON

--- public/main.js
+++ public/main.js
@@ -85,7 +85,7 @@ fileInput.addEventListener('change', () => {
 
       // Loop through the item blocks and create HTML for each one
       itemBlocks.forEach(itemBlock => {
-        if (itemBlock && itemBlock.length > 0 && itemBlock[0].ItemId && itemBlock[0].TotalCount) {
+        if (itemBlock && itemBlock.length > 0 && itemBlock.ItemId && itemBlock.TotalCount) {
           // Get Item ID
           const itemId = itemBlock.ItemId;
           console.log(`itemId: ${itemId}`);
